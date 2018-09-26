90fa91b1dd40928871065194dc6a0961accc04f0
dotzipfile <davenickmalone@gmail.com>
2018-09-26 21:50:13 +0100

    Implemented google map with search function, started implementing secure way of getting maps API Key

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-console.log('Starting lunch planner application');
-
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const httpPort = 3000;
 
+console.log('Starting lunch planner application...');
+
+console.log('Fetching Google API Key...');
+const keys = fs.readFileSync(__dirname + '/res/keys.json', 'utf-8');
+
 app.use(express.static(__dirname + '/public'));
-app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+app.get('/getKeys', (req, res) => res.send(keys));
 
 app.listen(httpPort, () => console.log(`App listening on port ${httpPort}`));
