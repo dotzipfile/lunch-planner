8c1d82392846f01acb23b57f035c262e80d3f525
dotzipfile <davenickmalone@gmail.com>
2018-09-26 21:21:16 +0100

    Added initial landing page

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Starting lunch planner application');
+
+const express = require('express');
+const app = express();
+const httpPort = 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+app.listen(httpPort, () => console.log(`App listening on port ${httpPort}`));
